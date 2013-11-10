d06172d8ac80b489cc2b6db680a4167c0ddd95c8
Jasper Lu <repsajrules@gmail.com>
2013-11-10 14:49:33 -0800

    finally got the drawing of the map function down

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 #include "keyboard.h"
 #include "game_init.h"
 #include "xterm_control.h" 
+#include "events.h"
 
 int main() {
 
@@ -27,6 +28,7 @@ int main() {
 
     init_map(map, ROWS, COLS);
     Tower* arr;
+    i = 0;
     arr = malloc(sizeof(Tower) * 10);
 
     int row = 1;
@@ -62,9 +64,10 @@ int main() {
 	{
 	    xt_par2(XT_SET_ROW_COL_POS,row,--col);
 	}
-	else if (c == 'T' || c == 't'  && map[row][col] == 'X')
+	else if (c == 'T' || c == 't' && map[row][col] == 'X')
 	{
 	    putchar('T');
+	    map[row][col] == 'T';
 	    Tower A;
 	    A.x = col;
 	    A.y = row;
@@ -75,14 +78,17 @@ int main() {
 	xt_par0(XT_CLEAR_SCREEN);
 	xt_par2(XT_SET_ROW_COL_POS,1,1);
 
-	draw_map(map, ROWS, COLS);
+	draw_map(map, ROWS, COLS, arr, i);
 
 	move_mob(&MOBS[0]);
 	xt_par2(XT_SET_ROW_COL_POS, MOBS[0].y + 1,MOBS[0].x + 1);
 
 	putchar('M');
 
+	xt_par0(XT_CH_RED);
+
 	xt_par2(XT_SET_ROW_COL_POS, row,col);
+
 
 	usleep(1000000 / FPS);
     }
@@ -91,4 +97,5 @@ int main() {
     xt_par0(XT_CLEAR_SCREEN);
     xt_par2(XT_SET_ROW_COL_POS,1,1);
 
+    free(map);
 }
